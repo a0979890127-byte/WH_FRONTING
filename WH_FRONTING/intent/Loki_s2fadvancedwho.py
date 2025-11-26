0d3e6035b81e0960518b736db204ec1dc83789b5
a0979890127-byte <a0979890127@gmail.com>
2025-11-26 23:05:25 +0800

    Add files via upload

---
+++ WH_FRONTING/intent/Loki_s2fadvancedwho.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    Loki module for s2fadvancedwho
+
+    Input:
+        inputSTR      str,
+        utterance     str,
+        args          str[],
+        resultDICT    dict,
+        refDICT       dict,
+        pattern       str
+
+    Output:
+        resultDICT    dict
+"""
+
+from importlib.util import module_from_spec
+from importlib.util import spec_from_file_location
+from random import sample
+import json
+import os
+
+INTENT_NAME = "s2fadvancedwho"
+CWD_PATH = os.path.dirname(os.path.abspath(__file__))
+
+def import_from_path(module_name, file_path):
+    spec = spec_from_file_location(module_name, file_path)
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+MODULE_DICT = {
+    "Account": import_from_path("WH_FRONTING_lib_Account", os.path.join(os.path.dirname(CWD_PATH), "lib/Account.py")),
+    "LLM": import_from_path("WH_FRONTING_lib_LLM", os.path.join(os.path.dirname(CWD_PATH), "lib/LLM.py"))
+}
+"""
+Account 變數清單
+[變數] BASE_PATH         => 根目錄位置
+[變數] LIB_PATH          => lib 目錄位置
+[變數] INTENT_PATH       => intent 目錄位置
+[變數] REPLY_PATH        => reply 目錄位置
+[變數] ACCOUNT_DICT      => account.info 內容
+[變數] ARTICUT           => ArticutAPI (用法：ARTICUT.parse()。 #需安裝 ArticutAPI.)
+[變數] USER_DEFINED_FILE => 使用者自定詞典的檔案路徑
+[變數] USER_DEFINED_DICT => 使用者自定詞典內容
+"""
+REPLY_PATH = MODULE_DICT["Account"].REPLY_PATH
+ACCOUNT_DICT = MODULE_DICT["Account"].ACCOUNT_DICT
+ARTICUT = MODULE_DICT["Account"].ARTICUT
+USER_DEFINED_FILE = MODULE_DICT["Account"].USER_DEFINED_FILE
+USER_DEFINED_DICT = MODULE_DICT["Account"].USER_DEFINED_DICT
+getLLM = MODULE_DICT["LLM"].getLLM
+
+# userDefinedDICT (Deprecated)
+# 請使用 Account 變數 USER_DEFINED_DICT 代替
+#userDefinedDICT = {}
+#try:
+#    userDefinedDICT = json.load(open(os.path.join(CWD_PATH, "USER_DEFINED.json"), encoding="utf-8"))
+#except:
+#    pass
+
+replyDICT = {}
+replyPathSTR = os.path.join(REPLY_PATH, "reply_{}.json".format(INTENT_NAME))
+if os.path.exists(replyPathSTR):
+    try:
+        replyDICT = json.load(open(replyPathSTR, encoding="utf-8"))
+    except Exception as e:
+        print("[ERROR] reply_{}.json => {}".format(INTENT_NAME, str(e)))
+CHATBOT = True if replyDICT else False
+
+# 將符合句型的參數列表印出。這是 debug 或是開發用的。
+def debugInfo(inputSTR, utterance):
+    if ACCOUNT_DICT["debug"]:
+        print("[{}] {} ===> {}".format(INTENT_NAME, inputSTR, utterance))
+
+def getReply(utterance, args):
+    replySTR = ""
+    try:
+        replySTR = sample(replyDICT[utterance], 1)[0]
+        if args:
+            replySTR = replySTR.format(*args)
+    except:
+        pass
+
+    return replySTR
+
+getResponse = getReply
+
+def getResult(inputSTR, utterance, args, resultDICT, refDICT, pattern="", toolkitDICT={}):
+    debugInfo(inputSTR, utterance)
+
+    # 確保 resultDICT["FT"] 存在
+    if "FT" not in resultDICT:
+        resultDICT["FT"] = []
+
+    if utterance == "李四不敢開王小明的刀":
+        if CHATBOT:
+            replySTR = getReply(utterance, args)
+            if replySTR:
+                resultDICT["response"] = replySTR
+                resultDICT["source"] = "reply"
+        else:
+            resultDICT["FT"].append("誰" + inputSTR.replace(args[0], ""))
+
+    if utterance == "李四從來不看批評王小明的文章":
+        if CHATBOT:
+            replySTR = getReply(utterance, args)
+            if replySTR:
+                resultDICT["response"] = replySTR
+                resultDICT["source"] = "reply"
+        else:
+            resultDICT["FT"].append("誰" + inputSTR.replace(args[0], ""))
+
+    if utterance == "李四相信因為老闆罵了張三，每個員工都很生氣":
+        if CHATBOT:
+            replySTR = getReply(utterance, args)
+            if replySTR:
+                resultDICT["response"] = replySTR
+                resultDICT["source"] = "reply"
+        else:
+            resultDICT["FT"].append("誰" + inputSTR.replace(args[0], ""))
+
+    if utterance == "李四連這本書都不肯給張三":
+        if CHATBOT:
+            replySTR = getReply(utterance, args)
+            if replySTR:
+                resultDICT["response"] = replySTR
+                resultDICT["source"] = "reply"
+        else:
+            resultDICT["FT"].append("誰" + inputSTR.replace(args[0], ""))
+
+    if utterance == "瑪莉見過張三好幾次":
+        if CHATBOT:
+            replySTR = getReply(utterance, args)
+            if replySTR:
+                resultDICT["response"] = replySTR
+                resultDICT["source"] = "reply"
+        else:
+            resultDICT["FT"].append("誰" + inputSTR.replace(args[0], ""))
+
+    if utterance == "瑪麗不喜歡王小明":
+        if CHATBOT:
+            replySTR = getReply(utterance, args)
+            if replySTR:
+                resultDICT["response"] = replySTR
+                resultDICT["source"] = "reply"
+        else:
+            resultDICT["FT"].append("誰" + inputSTR.replace(args[0], ""))
+
+    if utterance == "瑪麗最喜歡張三":
+        if CHATBOT:
+            replySTR = getReply(utterance, args)
+            if replySTR:
+                resultDICT["response"] = replySTR
+                resultDICT["source"] = "reply"
+        else:
+            resultDICT["FT"].append("誰" + inputSTR.replace(args[0], ""))
+
+    if utterance == "老王在會見之前就解雇了張三":
+        if CHATBOT:
+            replySTR = getReply(utterance, args)
+            if replySTR:
+                resultDICT["response"] = replySTR
+                resultDICT["source"] = "reply"
+        else:
+            resultDICT["FT"].append("誰" + inputSTR.replace(args[0], ""))
+
+    return resultDICT
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    resultDICT = getResult("瑪麗最喜歡張三", "瑪麗最喜歡張三", ["瑪麗"], {"FT": []}, {})
+    pprint(resultDICT)
